2c1b75c44940c54267e609d3837cc18ec77272da
Nikita Drozhzhin <drozhzhin.n.e@mail.ru>
2019-02-19 09:23:37 +0300

    Angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PinchZoomModule } from './pinch-zoom/pinch-zoom.module'; 
+import { PinchZoomModule } from './pinch-zoom/pinch-zoom.module';
 //import { PinchZoomModule } from 'ngx-pinch-zoom'; 
 
 @NgModule({

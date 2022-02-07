0a38b8dfd4c54621ed83b04b2a433c6f21e2f4b4
Konstantin Schütte <KSchuette@meddv.de>
2022-02-07 12:20:00 +0100

    added .angular to gitignore and fixed unit test

--- projects/ngx-pinch-zoom/src/lib/pinch-zoom.component.ts
+++ projects/ngx-pinch-zoom/src/lib/pinch-zoom.component.ts
@@ -1,298 +1,297 @@
-import {ChangeDetectorRef, Component, ElementRef, HostBinding, HostListener, Input, OnDestroy, SimpleChanges} from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostBinding, HostListener, Input, OnDestroy, SimpleChanges } from '@angular/core';
 
-import {Properties} from './interfaces';
-import {defaultProperties, backwardCompatibilityProperties} from './properties';
-import {IvyPinch} from './ivypinch';
+import { Properties } from './interfaces';
+import { defaultProperties, backwardCompatibilityProperties } from './properties';
+import { IvyPinch } from './ivypinch';
 
 interface ComponentProperties extends Properties {
-    disabled?:boolean;
-    overflow?: "hidden" | "visible";
-    disableZoomControl?: "disable" | "never" | "auto";
-    backgroundColor?: string;
+  disabled?: boolean;
+  overflow?: 'hidden' | 'visible';
+  disableZoomControl?: 'disable' | 'never' | 'auto';
+  backgroundColor?: string;
 }
 
-export const _defaultComponentProperties:ComponentProperties = {
-    overflow: "hidden",
-    disableZoomControl: "auto",
-    backgroundColor: "rgba(0,0,0,0.85)"
-}
+export const _defaultComponentProperties: ComponentProperties = {
+  overflow: 'hidden',
+  disableZoomControl: 'auto',
+  backgroundColor: 'rgba(0,0,0,0.85)',
+};
 
 type PropertyName = keyof ComponentProperties;
 
 @Component({
-	selector: 'pinch-zoom, [pinch-zoom]',
-    exportAs: 'pinchZoom',
-    templateUrl: './pinch-zoom.component.html',
-    styleUrls: ['./pinch-zoom.component.sass']
+  selector: 'pinch-zoom, [pinch-zoom]',
+  exportAs: 'pinchZoom',
+  templateUrl: './pinch-zoom.component.html',
+  styleUrls: ['./pinch-zoom.component.sass'],
 })
-
 export class PinchZoomComponent implements OnDestroy {
-    pinchZoom: any;
-    _properties!: ComponentProperties;
-    defaultComponentProperties!: ComponentProperties;
-    zoomControlPositionClass: string | undefined;
-    _transitionDuration!:number;
-    _doubleTap!:boolean;
-    _doubleTapScale!:number;
-    _autoZoomOut!:boolean;
-    _limitZoom!:number | "original image size";
+  pinchZoom: any;
+  _properties!: ComponentProperties;
+  defaultComponentProperties!: ComponentProperties;
+  zoomControlPositionClass: string | undefined;
+  _transitionDuration!: number;
+  _doubleTap!: boolean;
+  _doubleTapScale!: number;
+  _autoZoomOut!: boolean;
+  _limitZoom!: number | 'original image size';
 
-    @Input('properties') set properties(value: ComponentProperties) {
-        if (value) {
-            this._properties = value;
-        }
+  @Input('properties') set properties(value: ComponentProperties) {
+    if (value) {
+      this._properties = value;
     }
-    get properties() {
-        return this._properties;
-    }
+  }
+  get properties() {
+    return this._properties;
+  }
 
-    // transitionDuration
-    @Input('transition-duration') set transitionDurationBackwardCompatibility(value: number) {
-        if (value) {
-            this._transitionDuration = value;
-        }
+  // transitionDuration
+  @Input('transition-duration') set transitionDurationBackwardCompatibility(value: number) {
+    if (value) {
+      this._transitionDuration = value;
     }
-    @Input('transitionDuration') set transitionDuration(value: number) {
-        if (value) {
-            this._transitionDuration = value;
-        }
+  }
+  @Input('transitionDuration') set transitionDuration(value: number) {
+    if (value) {
+      this._transitionDuration = value;
     }
-    get transitionDuration() {
-        return this._transitionDuration;
-    }
+  }
+  get transitionDuration() {
+    return this._transitionDuration;
+  }
 
-    // doubleTap
-    @Input('double-tap') set doubleTapBackwardCompatibility(value: boolean) {
-        if (value) {
-            this._doubleTap = value;
-        }
+  // doubleTap
+  @Input('double-tap') set doubleTapBackwardCompatibility(value: boolean) {
+    if (value) {
+      this._doubleTap = value;
     }
-    @Input('doubleTap') set doubleTap(value: boolean) {
-        if (value) {
-            this._doubleTap = value;
-        }
+  }
+  @Input('doubleTap') set doubleTap(value: boolean) {
+    if (value) {
+      this._doubleTap = value;
     }
-    get doubleTap() {
-        return this._doubleTap;
-    }
+  }
+  get doubleTap() {
+    return this._doubleTap;
+  }
 
-    // doubleTapScale
-    @Input('double-tap-scale') set doubleTapScaleBackwardCompatibility(value: number) {
-        if (value) {
-            this._doubleTapScale = value;
-        }
+  // doubleTapScale
+  @Input('double-tap-scale') set doubleTapScaleBackwardCompatibility(value: number) {
+    if (value) {
+      this._doubleTapScale = value;
     }
-    @Input('doubleTapScale') set doubleTapScale(value: number) {
-        if (value) {
-            this._doubleTapScale = value;
-        }
+  }
+  @Input('doubleTapScale') set doubleTapScale(value: number) {
+    if (value) {
+      this._doubleTapScale = value;
     }
-    get doubleTapScale() {
-        return this._doubleTapScale;
-    }
+  }
+  get doubleTapScale() {
+    return this._doubleTapScale;
+  }
 
-    // autoZoomOut
-    @Input('auto-zoom-out') set autoZoomOutBackwardCompatibility(value: boolean) {
-        if (value) {
-            this._autoZoomOut = value;
-        }
+  // autoZoomOut
+  @Input('auto-zoom-out') set autoZoomOutBackwardCompatibility(value: boolean) {
+    if (value) {
+      this._autoZoomOut = value;
     }
-    @Input('autoZoomOut') set autoZoomOut(value: boolean) {
-        if (value) {
-            this._autoZoomOut = value;
-        }
+  }
+  @Input('autoZoomOut') set autoZoomOut(value: boolean) {
+    if (value) {
+      this._autoZoomOut = value;
     }
-    get autoZoomOut() {
-        return this._autoZoomOut;
-    }
+  }
+  get autoZoomOut() {
+    return this._autoZoomOut;
+  }
 
-    // limitZoom
-    @Input('limit-zoom') set limitZoomBackwardCompatibility(value: number | "original image size") {
-        if (value) {
-            this._limitZoom = value;
-        }
+  // limitZoom
+  @Input('limit-zoom') set limitZoomBackwardCompatibility(value: number | 'original image size') {
+    if (value) {
+      this._limitZoom = value;
     }
-    @Input('limitZoom') set limitZoom(value: number | "original image size") {
-        if (value) {
-            this._limitZoom = value;
-        }
+  }
+  @Input('limitZoom') set limitZoom(value: number | 'original image size') {
+    if (value) {
+      this._limitZoom = value;
     }
-    get limitZoom() {
-        return this._limitZoom;
-    }
+  }
+  get limitZoom() {
+    return this._limitZoom;
+  }
 
-    @Input() disabled!: boolean;
-    @Input() disablePan!: boolean;
-    @Input() overflow!: "hidden" | "visible";
-    @Input() zoomControlScale!: number;
-    @Input() disableZoomControl!: "disable" | "never" | "auto";
-    @Input() backgroundColor!: string;
-    @Input() limitPan!: boolean;
-    @Input() minPanScale!: number;
-    @Input() minScale!: number;
-    @Input() listeners!: 'auto' | 'mouse and touch';
-    @Input() wheel!: boolean;
-    @Input() autoHeight!: boolean;
-    @Input() wheelZoomFactor!: number;
-    @Input() draggableImage!: boolean;
+  @Input() disabled!: boolean;
+  @Input() disablePan!: boolean;
+  @Input() overflow!: 'hidden' | 'visible';
+  @Input() zoomControlScale!: number;
+  @Input() disableZoomControl!: 'disable' | 'never' | 'auto';
+  @Input() backgroundColor!: string;
+  @Input() limitPan!: boolean;
+  @Input() minPanScale!: number;
+  @Input() minScale!: number;
+  @Input() listeners!: 'auto' | 'mouse and touch';
+  @Input() wheel!: boolean;
+  @Input() autoHeight!: boolean;
+  @Input() wheelZoomFactor!: number;
+  @Input() draggableImage!: boolean;
 
-    @HostBinding('style.overflow')
-    get hostOverflow() {
-        return this.properties['overflow'];
-    }
-    @HostBinding('style.background-color')
-    get hostBackgroundColor() {
-        return this.properties['backgroundColor'];
-    }
+  @HostBinding('style.overflow')
+  get hostOverflow() {
+    return this.properties['overflow'];
+  }
+  @HostBinding('style.background-color')
+  get hostBackgroundColor() {
+    return this.properties['backgroundColor'];
+  }
 
-    get isTouchScreen() {
-        var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
-        var mq = function(query:any) {
-            return window.matchMedia(query).matches;
-        }
+  get isTouchScreen() {
+    var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+    var mq = function (query: any) {
+      return window.matchMedia(query).matches;
+    };
 
-        if (('ontouchstart' in window)) {
-            return true;
-        }
-
-        // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-        // https://git.io/vznFH
-        var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
-        return mq(query);
+    if ('ontouchstart' in window) {
+      return true;
     }
 
-    get isDragging() {
-        return this.pinchZoom ? this.pinchZoom.isDragging() : undefined;
+    // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+    // https://git.io/vznFH
+    var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+    return mq(query);
+  }
+
+  get isDragging() {
+    return this.pinchZoom ? this.pinchZoom.isDragging() : undefined;
+  }
+
+  get isDisabled() {
+    return this.properties['disabled'];
+  }
+
+  get scale() {
+    return this.pinchZoom.scale;
+  }
+
+  get isZoomedIn() {
+    return this.scale > 1;
+  }
+
+  get scaleLevel() {
+    return Math.round(this.scale / this._zoomControlScale);
+  }
+
+  get maxScale() {
+    return this.pinchZoom.maxScale;
+  }
+
+  get isZoomLimitReached() {
+    return this.scale >= this.maxScale;
+  }
+
+  get _zoomControlScale() {
+    return this.getPropertiesValue('zoomControlScale');
+  }
+
+  constructor(private elementRef: ElementRef) {
+    this.defaultComponentProperties = this.getDefaultComponentProperties();
+    this.applyPropertiesDefault(this.defaultComponentProperties, {});
+  }
+
+  ngOnInit() {
+    this.initPinchZoom();
+
+    /* Calls the method until the image size is available */
+    this.detectLimitZoom();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    let changedProperties = this.getProperties(changes);
+    changedProperties = this.renameProperties(changedProperties);
+
+    this.applyPropertiesDefault(this.defaultComponentProperties, changedProperties);
+  }
+
+  ngOnDestroy() {
+    this.destroy();
+  }
+
+  initPinchZoom() {
+    if (this.properties['disabled']) {
+      return;
     }
 
-    get isDisabled() {
-        return this.properties['disabled'];
+    this.properties['element'] = this.elementRef.nativeElement.querySelector('.pinch-zoom-content');
+    this.pinchZoom = new IvyPinch(this.properties);
+  }
+
+  getProperties(changes: SimpleChanges) {
+    let properties: any = {};
+
+    for (var prop in changes) {
+      if (prop !== 'properties') {
+        properties[prop] = changes[prop].currentValue;
+      }
+      if (prop === 'properties') {
+        properties = changes[prop].currentValue;
+      }
+    }
+    return properties;
+  }
+
+  renameProperties(properties: any) {
+    for (var prop in properties) {
+      if (backwardCompatibilityProperties[prop]) {
+        properties[backwardCompatibilityProperties[prop]] = properties[prop];
+        delete properties[prop];
+      }
     }
 
-    get scale() {
-        return this.pinchZoom.scale;
+    return properties;
+  }
+
+  applyPropertiesDefault(defaultProperties: ComponentProperties, properties: ComponentProperties): void {
+    this.properties = Object.assign({}, defaultProperties, properties);
+  }
+
+  toggleZoom() {
+    this.pinchZoom.toggleZoom();
+  }
+
+  isControl() {
+    if (this.isDisabled) {
+      return false;
     }
 
-    get isZoomedIn() {
-        return this.scale > 1;
+    if (this.properties['disableZoomControl'] === 'disable') {
+      return false;
     }
 
-    get scaleLevel() {
-        return Math.round(this.scale / this._zoomControlScale);
+    if (this.isTouchScreen && this.properties['disableZoomControl'] === 'auto') {
+      return false;
     }
 
-    get maxScale() {
-        return this.pinchZoom.maxScale;
+    return true;
+  }
+
+  detectLimitZoom() {
+    if (this.pinchZoom) {
+      this.pinchZoom.detectLimitZoom();
     }
+  }
 
-    get isZoomLimitReached() {
-        return this.scale >= this.maxScale;
+  destroy() {
+    if (this.pinchZoom) this.pinchZoom.destroy();
+  }
+
+  getPropertiesValue(propertyName: PropertyName) {
+    if (this.properties && this.properties[propertyName]) {
+      return this.properties[propertyName];
+    } else {
+      return this.defaultComponentProperties[propertyName];
     }
+  }
 
-    get _zoomControlScale() {
-        return this.getPropertiesValue('zoomControlScale');
-    }
-
-   constructor(private elementRef: ElementRef) {
-        this.defaultComponentProperties = this.getDefaultComponentProperties();
-        this.applyPropertiesDefault(this.defaultComponentProperties, {});
-    }
-
-    ngOnInit(){
-        this.initPinchZoom();
-        
-        /* Calls the method until the image size is available */
-        this.detectLimitZoom();
-    }
-
-    ngOnChanges(changes:SimpleChanges) {
-        let changedProperties = this.getProperties(changes);
-        changedProperties = this.renameProperties(changedProperties);
-
-        this.applyPropertiesDefault(this.defaultComponentProperties, changedProperties);
-    }
-
-    ngOnDestroy() {
-        this.destroy();
-    }
-
-    initPinchZoom() {
-        if (this.properties['disabled']) {
-            return;
-        }
-
-        this.properties['element'] = this.elementRef.nativeElement.querySelector('.pinch-zoom-content');
-        this.pinchZoom = new IvyPinch(this.properties);
-    }
-
-    getProperties(changes:SimpleChanges) {
-        let properties:any = {};
-
-        for (var prop in changes) {
-            if (prop !== 'properties'){
-                properties[prop] = changes[prop].currentValue;
-            }
-            if (prop === 'properties'){
-                properties = changes[prop].currentValue;
-            }
-        }
-        return properties;
-    }
-
-    renameProperties(properties: any) {
-        for (var prop in properties) {
-            if (backwardCompatibilityProperties[prop]) {
-                properties[backwardCompatibilityProperties[prop]] = properties[prop];
-                delete properties[prop];
-            }
-        }
-
-        return properties;
-    }
-
-    applyPropertiesDefault(defaultProperties:ComponentProperties, properties:ComponentProperties): void {
-        this.properties = Object.assign({}, defaultProperties, properties);
-    }
-
-    toggleZoom() {
-        this.pinchZoom.toggleZoom();
-    }
-
-    isControl() {
-        if (this.isDisabled) {
-            return false;
-        }
-
-        if (this.properties['disableZoomControl'] === "disable") {
-            return false;
-        }
-
-        if (this.isTouchScreen && this.properties['disableZoomControl'] === "auto") {
-            return false;
-        }
-
-        return true;
-    }
-
-    detectLimitZoom() {
-        if (this.pinchZoom) {
-            this.pinchZoom.detectLimitZoom();
-        }
-    }
-
-    destroy() {
-        this.pinchZoom.destroy();
-    }
-
-    getPropertiesValue(propertyName:PropertyName) {
-        if (this.properties && this.properties[propertyName]) {
-            return this.properties[propertyName]
-        } else {
-            return this.defaultComponentProperties[propertyName];
-        }
-    }
-
-    getDefaultComponentProperties() {
-        return {...defaultProperties, ..._defaultComponentProperties};
-    }
+  getDefaultComponentProperties() {
+    return { ...defaultProperties, ..._defaultComponentProperties };
+  }
 }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PinchZoomModule } from '../../projects/ngx-pinch-zoom/src/lib/pinch-zoom.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, PinchZoomModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -24,12 +21,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ivypinchApp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ivypinchApp app is running!');
   });
 });
